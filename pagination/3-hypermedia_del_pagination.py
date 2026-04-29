dbd0291ecdd92ef82844a1c5ad9ae369931cbd7f
Fai AlSharekh <fai.alsharekh@gmail.com>
2026-04-29 18:47:47 +0300

    create new file

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import csv
+from typing import List, Dict
+
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        indexed_data = self.indexed_dataset()
+        assert index is not None and 0 <= index < len(self.dataset())
+
+        data = []
+        current_index = index
+        
+        while len(data) < page_size and current_index < len(self.dataset()):
+            item = indexed_data.get(current_index)
+            if item:
+                data.append(item)
+            current_index += 1
+
+        return {
+            "index": index,
+            "data": data,
+            "page_size": page_size,
+            "next_index": current_index
+        }
