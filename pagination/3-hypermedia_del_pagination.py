4419eadd9121fb9ae4fe1de5608a4de27e17bf01
Fai AlSharekh <fai.alsharekh@gmail.com>
2026-04-29 18:51:20 +0300

    update

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
 import csv
 from typing import List, Dict
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -11,14 +16,19 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             self.__indexed_dataset = {
@@ -27,15 +37,21 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Deletion-resilient hypermedia pagination logic.
+        """
+        if index is None:
+            index = 0
+
         indexed_data = self.indexed_dataset()
-        assert index is not None and 0 <= index < len(self.dataset())
+        assert 0 <= index < len(self.dataset())
 
         data = []
         current_index = index
 
         while len(data) < page_size and current_index < len(self.dataset()):
             item = indexed_data.get(current_index)
-            if item:
+            if item is not None:
                 data.append(item)
             current_index += 1
 
