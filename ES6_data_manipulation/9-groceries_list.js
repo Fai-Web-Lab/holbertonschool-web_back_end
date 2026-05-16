686e0c72c7043fed0b5757f6fc735c6ff18de1b2
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-05-16 22:50:11 +0300

    create

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+/**
+ * Creates a map of groceries with specific items and quantities.
+ * 
+ * @returns {Map} A Map containing the grocery items.
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
