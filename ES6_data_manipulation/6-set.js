bf34e38dd238a3282e9eb17e00cc3afb1c92f140
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-05-16 22:42:43 +0300

    create

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a Set from an array.
+ * 
+ * @param {Array} array - The array to be converted into a Set.
+ * @returns {Set} A new Set containing the unique elements from the array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
