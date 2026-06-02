999ee17bdddbd67dc0ba2f12f95fa5a782ab26d6
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-06-02 16:19:49 +0300

    create

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,10 +1,33 @@
-const express = require('express');
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-const app = express();
 const PORT = 1245;
+const DATABASE = process.argv[2];
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const oldLog = console.log;
+      let output = [];
+      console.log = (msg) => output.push(msg);
+
+      await countStudents(DATABASE);
+
+      console.log = oldLog;
+      res.end(output.join('\n'));
+    } catch (error) {
+      res.end(error.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
 app.listen(PORT);
